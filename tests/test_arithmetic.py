5f4a7fc91e1d44e5a5cb6725f90d60b0ec200c10
Jacob Cannon <tug33296@temple.edu>
2020-12-10 21:41:22 -0500

    test

---
+++ tests/test_arithmetic.py
@@ -0,0 +1,15 @@
+import pytest
+
+def add(x, y):
+	return x + y
+
+def subtract(x, y):
+	return x - y
+
+def test_add():
+	assert add(3, 5) == 8
+
+def test_subtract():
+	dummy = 0
+	assert(5, 10) == 5
+
